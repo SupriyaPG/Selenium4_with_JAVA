2f41f38e06bb77193982394ffa3eeb1e5686cb7d
SupriyaPG <supriyapg369@gmail.com>
2025-04-04 19:36:10 +0530

    Assignments 04-04-25

---
+++ src/test/java/com/spgtesting/Assignments/Assignment_applitools.java
@@ -0,0 +1,50 @@
+package com.spgtesting.Assignments;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assignment_applitools {
+
+    @Test
+    @Description("Calculate the Total Amount of Money Spent ")
+    @Owner("Supriya G")
+    public void test_applitools(){
+
+        WebDriver driver=new EdgeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(" https://demo.applitools.com");
+
+        driver.findElement(By.id("username")).sendKeys("Admin");
+        driver.findElement(By.id("password")).sendKeys("Password@123");
+        driver.findElement(By.id("log-in")).click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://demo.applitools.com/app.html");
+
+        List<WebElement> amount=driver.findElements(By.xpath("//td[last()]"));
+        double totalamount=0.0;
+
+        for (WebElement amt:amount)
+        {
+            String actualAmt = amt.getText().replace("+", "").replace("USD", "").replace(",","").replace("-","").trim();
+            System.out.println(actualAmt);
+            double doubleAmt=Double.parseDouble(actualAmt);
+            totalamount +=doubleAmt;
+        }
+
+        System.out.println("Total spending : "+totalamount);
+
+        Assert.assertEquals(totalamount,"1996.22","Total amount mismatched");
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/spgtesting/Assignments/Assignment_demoqa.java
@@ -0,0 +1,37 @@
+package com.spgtesting.Assignments;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Assignment_demoqa {
+
+    @Test
+    @Description()
+    @Owner("Supriya G")
+    public void test_Edit_button(){
+
+        WebDriver driver=new EdgeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/webtables");
+        driver.findElement(By.id("addNewRecordButton")).click();
+        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Meena");
+        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("More");
+        driver.findElement(By.xpath("//input[@placeholder='name@example.com']")).sendKeys("meena@example.com");
+        driver.findElement(By.xpath("//input[@id='age']")).sendKeys("35");
+        driver.findElement(By.xpath("//input[@id='salary']")).sendKeys("25000");
+        driver.findElement(By.xpath("//input[@placeholder='Department']")).sendKeys("HR");
+        driver.findElement(By.id("submit")).click();
+        String name=driver.findElement(By.xpath(" //div[normalize-space()='Meena']")).getText();
+
+        Assert.assertEquals(name,"Meena");
+
+        driver.quit();
+
+    }
+}
