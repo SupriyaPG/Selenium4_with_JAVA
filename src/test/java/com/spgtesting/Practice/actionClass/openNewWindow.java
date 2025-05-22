6b0d35cde6bff33fc9b30f18875b31f002f8e531
SupriyaPG <supriyapg369@gmail.com>
2025-05-22 20:28:35 +0530

    Keyboard actions class practice

--- src/test/java/com/spgtesting/Practice/actionClass/openNewWindow.java
+++ src/test/java/com/spgtesting/Practice/actionClass/openNewWindow.java
@@ -1,11 +1,15 @@
 package com.spgtesting.Practice.actionClass;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class openNewWindow {
     public static void main(String[] args) {
@@ -17,7 +21,21 @@ public class openNewWindow {
         driver.manage().window().maximize();
 
         Actions act=new Actions(driver);
-        //act.keyDown(Keys.CONTROL).sendKeys()
+
+        WebElement myAccount=driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/div[1]/div[2]/ul[1]/li[2]/a[1]/span[1]"));
+        act.keyDown((Keys.CONTROL)).click(myAccount).keyUp(Keys.CONTROL).perform();
+        WebElement regLink=driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/div[1]/div[2]/ul[1]/li[2]/ul[1]/li[1]/a[normalize-space()='Register']"));
+        act.keyDown(Keys.CONTROL).click(regLink).keyUp(Keys.CONTROL).perform();
+
+        List<String> ids=new ArrayList(driver.getWindowHandles());
+        driver.switchTo().window(ids.get(1));
+
+        WebElement registrationPage=driver.findElement(By.cssSelector("div[id='content'] h1"));
+        if(registrationPage.equals("Register Account"))
+        {
+            System.out.println(registrationPage);
+        }
+
         driver.quit();
 
 
